4e45d0c6d178618fd8ff27da83f762ad92099067
Manisha B <bholemr@gmail.com>
2022-01-25 17:15:12 -0500

    getArea() method added

--- src/org/launchcode/java/studios/areaofacircle/Circle.java
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.studios.areaofacircle;
 
 public class Circle {
+    //Add a method
     public static Double getArea(Double radius) {
         return 3.14 * radius * radius;
     }
