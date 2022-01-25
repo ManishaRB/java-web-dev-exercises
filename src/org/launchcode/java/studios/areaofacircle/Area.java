59f7d0f8c2a624287696917660bc0740e79d8b3e
Manisha B <bholemr@gmail.com>
2022-01-25 17:10:17 -0500

    Bonus mission completed

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -2,20 +2,29 @@ package org.launchcode.java.studios.areaofacircle;
 
 import java.util.Scanner;
 
-
 public class Area {
     public static void main(String args[]){
-        Scanner input;
-        double radius, area;
+        double radius = 0;
+        double area;
+        boolean isNumber;
         //final double PI = 3.14;
-        input = new Scanner(System.in);
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius : ");
         do {
-            System.out.println("Enter a radius : ");
-            //str.matches("-?\\d+(\\.\\d+)?")
-            radius = input.nextDouble();
-        }while (!(radius > 0 ) );
-       // area = PI * radius * radius;
+            if (input.hasNextDouble()) {   //returns true if the next token in this scanner’s input can be interpreted as a Double using the nextDouble() method
+                radius = input.nextDouble();
+                isNumber = true;
+                if (radius < 0) {
+                    System.out.println("Please enter a positive number");
+                    radius =input.nextDouble();
+                }
+            } else {
+                System.out.println("Please enter a valid number ");
+                isNumber = false;
+                input.next();
+            }
+        }while (!(isNumber) );
+        //call to getArea() from class Circle
         area = Circle.getArea(radius);
         System.out.println("The area of a circle of radius " + radius + " is:" +area);
 
